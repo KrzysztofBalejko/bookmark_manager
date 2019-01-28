11972eff2c27ab8a59735e7997d112d9337b284d
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-28 21:44:14 +0000

    Refactoring /bookmarks to use Bookmark class

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
 
@@ -7,14 +8,8 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/bookmarks' do
-
-    @bookmarks = [
-              "http://www.google.com",
-              "http://www.reuters.com",
-              "http://www.bbc.co.uk"
-              ]
-    erb :'bookmarks/index'
-
+    @bookmarks = Bookmark.all
+    erb(:'bookmarks/index')
   end
 
   run! if app_file == $0
