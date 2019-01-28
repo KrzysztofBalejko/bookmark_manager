4748df85a30227ab9ab578cbe196bd4c94596133
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-28 17:55:33 +0000

    Controller updated - passing the test

--- app.rb
+++ app.rb
@@ -6,5 +6,16 @@ class BookmarkManager < Sinatra::Base
     'Bookmark Manager'
   end
 
+  get '/bookmarks' do
+
+    @bookmarks = [
+              "http://www.google.com",
+              "http://www.reuters.com",
+              "http://www.bbc.co.uk"
+              ]
+    erb :'bookmarks/index'
+
+  end
+
   run! if app_file == $0
 end
