fb2921b40090b6c8348595f2d72b930723b21a84
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-30 14:12:53 +0000

    Changes to the feature test. Truncating bookmarks

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -9,9 +9,15 @@ end
 
 feature 'viewing bookmarks' do
   scenario 'A user can see bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.google.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.reuters.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.bbc.com');")
+
     visit('/bookmarks')
-    expect(page).to have_content("http://www.google.com")
-    expect(page).to have_content("http://www.reuters.com")
-    expect(page).to have_content("http://www.bbc.com")
+    expect(page).to have_content('http://www.google.com')
+    expect(page).to have_content('http://www.reuters.com')
+    expect(page).to have_content('http://www.bbc.com')
   end
 end
