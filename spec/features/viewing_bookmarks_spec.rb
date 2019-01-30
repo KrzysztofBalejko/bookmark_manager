a7b861fb2f8e4ac13f5a5f214b33c82ab49cf79c
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-30 21:13:56 +0000

    Test refactoring

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -10,11 +10,9 @@ end
 feature 'viewing bookmarks' do
   scenario 'A user can see bookmarks' do
     connection = PG.connect(dbname: 'bookmark_manager_test')
-
-    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.google.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.reuters.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.bbc.com');")
-
+    Bookmark.create(url: 'http://www.google.com')
+    Bookmark.create(url: 'http://www.reuters.com')
+    Bookmark.create(url: 'http://www.bbc.com')
     visit('/bookmarks')
     expect(page).to have_content('http://www.google.com')
     expect(page).to have_content('http://www.reuters.com')
