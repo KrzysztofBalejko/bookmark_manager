78a718ae50b76ee537060af458025e35b35a0ce7
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-01 13:27:17 +0000

    Delete functionality added to controller

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
 
+  enable :sessions, :method_override
+
   get '/' do
     'Bookmark Manager'
   end
@@ -20,6 +22,14 @@ class BookmarkManager < Sinatra::Base
     Bookmark.create(url: params['url'], title: params['title'])
     redirect '/bookmarks'
   end
+
+  delete '/bookmarks/:id' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("DELETE FROM bookmarks WHERE id = #{params['id']}")
+    redirect '/bookmarks'
+  end
+
+
 
   run! if app_file == $0
 end
