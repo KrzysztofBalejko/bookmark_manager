6119eb789c4da5542e24d5733fec4f8e37c8fdea
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-01 15:32:41 +0000

    Minor changes

--- spec/features/updating_a_bookmark_spec.rb
+++ spec/features/updating_a_bookmark_spec.rb
@@ -10,7 +10,6 @@ feature 'Updating a bookmark' do
     fill_in('url', with: "http://www.snakersacademy.com")
     fill_in('title', with: "Snakers Academy")
     click_button('Submit')
-
     expect(current_path).to eq '/bookmarks'
     expect(page).not_to have_link('Makers Academy', href: 'http://www.makersacademy.com')
     expect(page). to have_link('Snakers Academy', href: 'http://www.snakersacademy.com')
